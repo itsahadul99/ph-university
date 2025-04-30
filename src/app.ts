7ba49907d215d54356fcfdef931c0f1fc1818e9a
itsahadul99 <itsahadul999@gmail.com>
2025-04-30 12:31:24 +0600

    Update: added get all students and single student

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/config/modules/student/student.route'
 const app: Application = express()
 app.use(express.json())
 app.use(cors())
 
+// application routes
+app.use("/api/v1/students", StudentRoutes)
+
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send('Hello from server')
 })
 
 export default app;
--- src/app/config/modules/student/student.controller.ts
+++ src/app/config/modules/student/student.controller.ts
@@ -1,9 +1,35 @@
 import { Request, Response } from "express";
 import { StudentServices } from "./student.service";
 
+const getAllStudents = async (req: Request, res: Response) => {
+    try {
+        const result = await StudentServices.getAllStudentsFromDB();
+        res.status(200).json({
+            success: true,
+            message: "Student are retrieved successfully",
+            data: result
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+const getSingleStudent = async (req: Request, res: Response) => {
+    try {
+        const { studentId } = req.params;
+        const result = await StudentServices.getSingleStudentFromDB(studentId);
+        res.status(200).json({
+            success: true,
+            message: "Student is retrieved successfully",
+            data: result
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const createStudent = async (req: Request, res: Response) => {
     try {
-        const student = req.body;
+        const { student } = req.body;
         // will call the service func to send this data
         const result = await StudentServices.createStudentIntoDB(student)
         // send the res
@@ -19,4 +45,6 @@ const createStudent = async (req: Request, res: Response) => {
 
 export const StudentControllers = {
     createStudent,
+    getAllStudents,
+    getSingleStudent
 }
