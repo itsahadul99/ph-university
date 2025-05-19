0a2705299aa3babb80f70b13ed6ce1e12d2ae30f
itsahadul99 <itsahadul999@gmail.com>
2025-05-19 17:15:53 +0600

    Update: refactor routing by consolidating student and user routes into a dedicated router module

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
-import { StudentRoutes } from './app/config/modules/student/student.route'
-import { UserRoutes } from './app/config/modules/user/user.route'
 import { globalErrorHandler, notFoundHandler } from './app/middlewares'
+import router from './app/routes'
 const app: Application = express()
 app.use(express.json())
 app.use(cors())
 
 // application routes
-app.use("/api/v1/students", StudentRoutes)
-app.use("/api/v1/users", UserRoutes)
 
-
+app.use('/api/v1', router)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello from server')
