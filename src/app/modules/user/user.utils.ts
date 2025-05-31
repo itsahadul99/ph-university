6b0a1ecc707dac17da4eb4dc7054e4feee1c534c
itsahadul99 <itsahadul99@gmail.com>
2025-05-31 16:01:15 +0600

    Update: fix student ID retrieval logic to return full ID and ensure correct ID generation

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -13,12 +13,19 @@ const findLastStudentId = async () => {
         }
     ).sort({ createdAt: -1 }).lean()
 
-    return lastStudentId?.id ? lastStudentId?.id.substring(6) : undefined
+    return lastStudentId?.id ? lastStudentId?.id : undefined
 }
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
     // first time when no student
-    const currentId = await findLastStudentId() || (0).toString();
+    let currentId = (0).toString();
+    const lastStudentId = await findLastStudentId();
+    const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+    const lastStudentYear = lastStudentId?.substring(0, 4);
+
+    if (lastStudentId && lastStudentSemesterCode === payload.code && lastStudentYear === payload.year) {
+        currentId = lastStudentId.substring(6)
+    }
     let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
     incrementId = `${payload.year}${payload.code}${incrementId}`
     return incrementId;
