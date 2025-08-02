bbee165b6cbca25450ca859d75fbf1f12afe441f
itsahadul99 <itsahadul999@gmail.com>
2025-08-02 12:47:48 +0600

    feat: enhance updateCourseIntoDB to manage pre-requisite courses addition and deletion

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -12,8 +12,30 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
     //step 1: Update Basic Course Data
     const updatedBasicCourseData = await Course.findByIdAndUpdate(
         id, courseRemainingData, { new: true, runValidators: true }
-    ).populate('preRequisiteCourses.course');
-    return updatedBasicCourseData;
+    )
+    if (preRequisiteCourses && preRequisiteCourses?.length > 0) {
+        //step 2: Update Pre-Requisite Courses
+        // filter out deleted pre-requisite courses
+        const deletedPreRequisiteCoursesId = preRequisiteCourses?.filter(course => course.isDeleted && course.isDeleted).map(course => course.course);
+        const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
+            $pull: {
+                preRequisiteCourses: { course: { $in: deletedPreRequisiteCoursesId } }
+            }
+        })
+        const updatedPreRequisiteCoursesId = preRequisiteCourses?.filter(course => !course.isDeleted);
+        // Update existing pre-requisite courses
+        const updatedPreRequisiteCourses = await Course.findByIdAndUpdate(
+            id, {
+            $addToSet: {
+                preRequisiteCourses: {
+                    $each: updatedPreRequisiteCoursesId
+                }
+            }
+        }
+        );
+    }
+    const result = await Course.findById(id).populate('preRequisiteCourses.course');
+    return result;
 }
 const getAllCoursesFromDB = async (query: Record<string, unknown>) => {
     const courseQuery = new QueryBuilder(Course.find().populate('preRequisiteCourses.course'), query)
